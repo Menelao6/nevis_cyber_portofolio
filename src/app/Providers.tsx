21216c68cad9bc01717f77b3c702b6415f7431cc
Menelao6 <menelao.pone@gmail.com>
2025-08-10 21:29:22 +0200

    Delete src directory

--- src/app/Providers.tsx
+++
@@ -1,9 +0,0 @@
-// app/Providers.tsx
-'use client';
-
-import { ReactNode } from 'react';
-import { ThemeProvider } from './components/ThemeProvider/ThemeProvider';
-
-export default function Providers({ children }: { children: ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
-}
--- src/app/types/react-is.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'react-is' {
-  export function isValidElementType(obj: any): boolean;
-}
